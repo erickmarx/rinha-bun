9bb87966b37c3601663d66bda276364ec7769c6e
erickmarx <erickmarx5@gmail.com>
2024-02-14 16:21:34 -0300

    fix: postgresql conf

--- init.sql
+++ init.sql
@@ -1,6 +1,5 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -8,7 +7,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 SET default_tablespace = '';
-SET default_table_access_method = heap;
 
 CREATE UNLOGGED TABLE customers (
 	id SERIAL PRIMARY KEY,
